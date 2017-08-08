f262e4be9be5531f9bbbfe9377a5ded137d775bd
Benjamin Caspari <caspari@rossmanith.com>
2017-08-08 16:52:27 +0200

    typings

--- src/process/types/bpmn-moddle/lib/bpmn-moddle.d.ts
+++ src/process/types/bpmn-moddle/lib/bpmn-moddle.d.ts
@@ -72,6 +72,7 @@ declare module "bpmn-moddle/lib/bpmn-moddle" {
     public create(descriptor: "bpmn:TimerEventDefinition", attrs: Partial<Bpmn.TimerEventDefinition>): Bpmn.TimerEventDefinition;
     public create(descriptor: "bpmn:UserTask", attrs: Partial<Bpmn.UserTask>): Bpmn.UserTask;
     public create(descriptor: "bpmn:TextAnnotation", attrs: Partial<Bpmn.TextAnnotation>): Bpmn.TextAnnotation;
+    public create(descriptor: "bpmn:Association", attrs: Partial<Bpmn.Association>): Bpmn.Association;
 
     public create(descriptor: "bpmndi:BPMNDiagram", attrs: Partial<Bpmndi.BPMNDiagram>): Bpmndi.BPMNDiagram;
     public create(descriptor: "bpmndi:BPMNEdge", attrs: Partial<Bpmndi.BPMNEdge>): Bpmndi.BPMNEdge;
