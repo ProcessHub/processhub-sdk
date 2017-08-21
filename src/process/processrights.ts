8ce5403a175d810c123d76212a73b506e0d8fad7
Benjamin Caspari <caspari@rossmanith.com>
2017-08-21 14:48:29 +0200

    bug fix: starting roles

--- src/process/processrights.ts
+++ src/process/processrights.ts
@@ -88,12 +88,12 @@ export function getProcessRoles(currentRoles: PH.Process.ProcessRoles, bpmnProce
       }
       processRoles[lane.id].roleName = lane.name;
     });
+
+    // set starting roles
     const startLanes: string[] = bpmnProcess.getStartLaneIds();
-    if (startLanes) {
-      for (const startLane of startLanes) {
-        processRoles[startLane].isStartingRole = true;
-      }
-    }      
+    for (const role in processRoles) {
+      processRoles[role].isStartingRole = (startLanes && (undefined !== startLanes.find(s => s === role)));
+    }
 
     // Im Prozess nicht mehr vorhandene Lanes entfernen
     for (let role in processRoles) {
