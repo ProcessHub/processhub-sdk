e522ecc8b75c39e81e649b41325fc91fa2fedd51
Benjamin Caspari <caspari@rossmanith.com>
2017-08-23 15:31:52 +0200

    date as form field

--- src/data/datainterfaces.ts
+++ src/data/datainterfaces.ts
@@ -1,5 +1,23 @@
 import * as PH from "../";
 
+export interface FieldValue {
+  type: string;
+  value: Date | string | boolean;
+}
+
+/**
+ * Check if an element implements the FieldValue interface
+ * @param element element to check
+ * @return {boolean} true, if element implements the FieldValue interface, false otherwise
+ */
+export function isFieldValue(element: {}): element is FieldValue {
+  return element
+    && (element as FieldValue).type !== undefined    
+    && typeof (element as FieldValue).type === "string"
+    && (element as FieldValue).value !== undefined;
+}
+
+
 export interface FieldContentMap {
-  [fieldId: string]: string | boolean;
+  [fieldId: string]: string | boolean | FieldValue;
 }
