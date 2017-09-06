10d92418a2aaa59f20c99aa35491bcc21dac18e0
Tilo Rossmanith <tr@rossmanith.com>
2017-09-06 17:04:27 +0200

    mailer restructured

--- src/mailer/mailerinterfaces.ts
+++ src/mailer/mailerinterfaces.ts
@@ -1,11 +1,20 @@
 import * as PH from "../";
 
-export interface MessageNotificationMailContent {
+export enum MailSender {
+  FromProcessHub,   // noreply@processhub.com
+  FromRoleOwner     // use the mail address of the current RoleOwner as ReplyTo-destination     
+}
+
+export interface MailContent {
+  signature?: string;
+}
+
+export interface MessageNotificationMailContent extends MailContent {
+  sender?: MailSender;
   todoLink: string;  
   todoTitle: string;
   todoDescription: string;
   fieldContents: PH.Data.FieldContentMap;
-
   subject: string;
   sendInstanceLink: boolean;
   sendFieldContents: boolean;
