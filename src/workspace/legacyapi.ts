d46eeab8fdf7af8b78fc5e1e82a7443735becec7
Tilo Rossmanith <tr@rossmanith.com>
2017-10-06 13:33:21 +0200

    GetWorkspacesForUser removed

--- src/workspace/legacyapi.ts
+++ src/workspace/legacyapi.ts
@@ -8,8 +8,7 @@ export const WorkspaceRequestRoutes = {
   RemoveWorkspaceMember: "/api/workspace/removemember",
   UpdateWorkspace: "/api/workspace/update",
   DeleteWorkspace: "/api/workspace/delete",
-  SetMemberRole: "/api/workspace/setmemberrole",
-  GetWorkspacesForUser: "/api/workspace/workspacesforuser"
+  SetMemberRole: "/api/workspace/setmemberrole"
 };
 export type WorkspaceRequestRoutes = keyof typeof WorkspaceRequestRoutes;
 
@@ -61,11 +60,4 @@ export interface SetMemberRoleRequest extends PH.LegacyApi.BaseRequest {
   workspaceId: string;
   userId: string;
   memberRole: PH.Workspace.WorkspaceRole;
-}
-
-export interface GetWorkspacesForUserRequest extends PH.LegacyApi.BaseRequest {
-  userId: string;
-}
-export interface GetWorkspacesForUserReply extends PH.LegacyApi.BaseMessage {
-  workspaces?: Array<PH.Workspace.WorkspaceDetails>;
 }
