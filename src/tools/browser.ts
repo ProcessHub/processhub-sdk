e1fbb61654e287e5c580dbc5a801065c2d7fedef
Tilo Rossmanith <tr@rossmanith.com>
2017-10-09 16:31:11 +0200

    fix: crash on server rendering

--- src/tools/browser.ts
+++ src/tools/browser.ts
@@ -1,7 +1,13 @@
 export function isMicrosoftEdge(): boolean {
+  if (typeof navigator == "undefined" || navigator == null)
+    return false;
+
   return /Edge\/1./i.test(navigator.userAgent);
 }
 
 export function isIE11(): boolean {
+  if (typeof navigator == "undefined" || navigator == null)
+    return false;
+
   return /Trident\/7\./.test(navigator.userAgent);
 }
