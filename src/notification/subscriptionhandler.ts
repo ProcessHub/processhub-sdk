08656dc84fd3a82b038b65fb546274fae922beb0
Tilo Rossmanith <tr@rossmanith.com>
2017-10-18 17:58:24 +0200

    subscribtion -> subscription

--- src/notification/subscriptionhandler.ts
+++ src/notification/subscriptionhandler.ts
@@ -5,24 +5,24 @@ import { UserDetails } from "../user/userinterfaces";
 
 export interface PublishSubscribeRegisterObject {
   wildcard: string;
-  subscribtionPath: string;
+  subscriptionPath: string;
   resolvePath: (value: string) => string;
 }
 
 export const resolveFunction = (obj: any, value: string): string => {
-  return obj.subscribtionPath.replace(obj.wildcard, value);
+  return obj.subscriptionPath.replace(obj.wildcard, value);
 };
 
-export const PublishSubscribtionObjects: { [Id: string]: PublishSubscribeRegisterObject } = {
-  newInstance: { wildcard: "{userId}", subscribtionPath: "/newInstance/{userId}", resolvePath: function (value: string) { return resolveFunction(this, value); } },
-  updateInstance: { wildcard: "{instanceId}", subscribtionPath: "/updateInstance/{instanceId}", resolvePath: function (value: string) { return resolveFunction(this, value); } },
+export const PublishSubscriptionObjects: { [Id: string]: PublishSubscribeRegisterObject } = {
+  newInstance: { wildcard: "{userId}", subscriptionPath: "/newInstance/{userId}", resolvePath: function (value: string) { return resolveFunction(this, value); } },
+  updateInstance: { wildcard: "{instanceId}", subscriptionPath: "/updateInstance/{instanceId}", resolvePath: function (value: string) { return resolveFunction(this, value); } },
 };
 
 let wsUrl = BACKEND_URL;
 wsUrl = wsUrl.replace("https://", "wss://");
 wsUrl = wsUrl.replace("http://", "ws://");
 
-let subscribtionPaths: string[] = [];
+let subscriptionPaths: string[] = [];
 let notificationClient = new Nes.Client(wsUrl);
 
 let notificationHandler = (update: any, flags: any) => {
@@ -39,24 +39,24 @@ export async function initNotificationClient(user: UserDetails) {
   });
 }
 
-export function subscribe(subscribtionPath: string): boolean {
-  notificationClient.subscribe(subscribtionPath, notificationHandler, (err: any) => {
+export function subscribe(subscriptionPath: string): boolean {
+  notificationClient.subscribe(subscriptionPath, notificationHandler, (err: any) => {
     if (err != null) {
-      console.log("Error on subscribe path: " + subscribtionPath);
+      console.log("Error on subscribe path: " + subscriptionPath);
       console.log(err);
       return false;
     }
   });
-  subscribtionPaths.push(subscribtionPath);
+  subscriptionPaths.push(subscriptionPath);
   return true;
 }
 
 export function subscribeUpdateInstance(instanceId: string): boolean {
-  let subPath = PublishSubscribtionObjects.updateInstance.resolvePath(instanceId);
+  let subPath = PublishSubscriptionObjects.updateInstance.resolvePath(instanceId);
   return subscribe(subPath);
 }
 
 export function subscribeNewInstance(userId: string): boolean {
-  let subPath = PublishSubscribtionObjects.newInstance.resolvePath(userId);
+  let subPath = PublishSubscriptionObjects.newInstance.resolvePath(userId);
   return subscribe(subPath);
 }
