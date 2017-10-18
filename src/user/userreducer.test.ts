be5cd12283e61d8d8915bc2f045561016a837b9b
Benjamin Caspari <caspari@rossmanith.com>
2017-10-18 14:48:25 +0200

    moved userreducer.test to SDK

---
+++ src/user/userreducer.test.ts
@@ -0,0 +1,47 @@
+import { assert } from "chai";
+import { UserState } from "./phclient";
+import { ApiResult } from "../legacyapi/apirequests";
+import { createUserId } from "../tools/guid";
+import { userReducer } from "./userreducer";
+import { UserActionLoggedIn, UserActionFailed } from "./useractions";
+import { UserActionsType } from "./userinterfaces";
+
+describe("common", function () {
+  describe("user", function () {
+    describe("userreducer", function () {
+
+      describe("userReducer", function () {
+
+        it("soll USERACTION_LOGIN korrekt reducen", function () {
+          let oldState: UserState = { lastApiResult: ApiResult.API_ERROR };
+          let user = { userId: createUserId() };
+          let newState = userReducer(oldState, <UserActionLoggedIn>{
+            type: UserActionsType.LoggedIn,
+            userDetails: user
+          });
+          assert.deepEqual<any>(newState, { currentUser: user, lastApiResult: ApiResult.API_OK });
+        });
+
+        it("soll USERACTION_FAILED korrekt reducen", function () {
+          let oldState: UserState = { lastApiResult: ApiResult.API_ERROR };
+          let newState = userReducer(oldState, <UserActionFailed>{
+            type: UserActionsType.Failed,
+            result: ApiResult.API_DENIED
+          });
+          assert.deepEqual<any>(newState, { lastApiResult: ApiResult.API_DENIED });
+        });
+
+        it("soll unbekannte Actions korrekt reducen", function () {
+          let oldState: UserState = { lastApiResult: ApiResult.API_ERROR };
+          let newState = userReducer(oldState, {
+            type: "UNKNOWN"
+          });
+          assert.deepEqual(newState, oldState);
+        });
+
+
+      });
+
+    });
+  });
+});
