05d8d16d35ef488111a7ad2f44f71a3e75b8d6e2
Tilo Rossmanith <tr@rossmanith.com>
2017-10-28 21:48:49 +0200

    requireAuthentication

--- src/user/usertools.ts
+++ src/user/usertools.ts
@@ -1,4 +1,16 @@
 import { UserDetails } from "./userinterfaces";
+import { CoreEnvironment } from "../environment";
+
+export function requireAuthentication(coreEnv: CoreEnvironment): void {
+  if (!coreEnv.user) {
+    // no user is logged in - redirect to signin
+    if (typeof window != "undefined") {  // otherwise fails on server rendering
+      window.location.href = "/signin?redirect=" + encodeURIComponent(window.location.pathname);
+    } else
+      // 401 will result in redirect in renderroute
+      throw { error: 401 };
+  }
+}
 
 // user.lanugage contains the preferred user language, even if it does not (yet) exist
 // in ProcessHub (e.g. "fr", if user is french)
